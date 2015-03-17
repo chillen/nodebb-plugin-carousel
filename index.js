350979ce57e3169e5261786cb9242ef1df6006b6
Connor Hillen <connor.hillen@gmail.com>
2015-03-16 21:14:28 -0400

    Updated for 0.6, lots of changes

--- index.js
+++ index.js
@@ -1,24 +1,81 @@
 (function(module) {
     var Carousel = {};
 
-    Carousel.parse = function(postContent, callback) {
-        var re = /\[carousel(.*)]/mg;
-        var arr = postContent.match(re);
-        if(arr && arr.length){
-            var config = arr[0].replace(re, '$1');
-            re = /(<br>|)(<img )src(=".*?>)(<br>|)/mg;
-            postContent = postContent.replace(re, '<div class="carousel-img">$2class="lazyOwl" data-src$3</div>');
-            re = /<div class="carousel-img.*?<\/div>/mg;
-            arr = postContent.match(re);
-            if(arr && arr.length){
-                postContent = postContent.replace(/\[carousel(.*)]/mg, '');
-                var firstItem = arr[0];
-                postContent = postContent.replace(firstItem, '<div class="carousel" config="'+config+'">'+firstItem);
-                re = /(<div class="carousel.*<\/div>)(.*[^<\/div>])/mg;
-                postContent = postContent.replace(re, '$1</div>$2')
+    // Searches for the regex [carousel] ... [/carousel]
+    var carRegex = /\[carousel[\s]*\{([\s\S]*?)\}\]([\s\S]*)\[\/carousel\]/;
+
+    // Searches for the regex [item] ... [/item]
+    var itemRegex = /\[item (.*)\]/mg;
+
+    var imgRegex = /\[item img-src="(.*)"\]/;
+
+    // This is the regex, if markdown plugin renders it as a link
+    var imgRegexMarkdown = /\[item img-src=\&quot;<a href="(.*)".*\&quot;\]/;
+
+
+
+    var generateCarousel = function( content ) {
+      var matched = carRegex.exec(content)
+        , config  = matched[1]
+        , content = matched[2]
+        , items   = itemRegex.exec(content)
+        , config  = config.replace(/&quot;/g, "'")
+        , str     = "<div class='owl-carousel owl-theme carousel' " + (config? "config={"+config+"}" : "") + ">"
+        ;
+
+        console.log(config);
+
+        // Items should be incoming as a JSON object
+        while(items) {
+          try {
+            str += "<div class='item'>";
+
+            // Prepare the JSON. Replace &quot; with quotes, remove any
+            // extra <a href>'s
+            var json = items[1].replace(/&quot;/g, "\"");
+            json = json.replace(/<a href=[\s\S]*?>(.*?)<\/a>/g, "$1");
+
+            console.log(json);
+            json = JSON.parse(json);
+
+            if (json.href) {
+              str += "<a href=" + json.href + ">";
             }
+            if (json.imgsrc) {
+              str += "<div class='carousel-img'><img class='carousel-img' src='"+json.imgsrc+"' /></div>";
+            }
+            if (json.caption) {
+              str += "<div class='carousel-caption'>"+json.caption+"</div>";
+            }
+            if (json.href) {
+              str += "</a>";
+            }
+
+            str += "</div>";
+            items = itemRegex.exec(content);
+          }
+          catch (e) {
+            console.error("ERROR PROCESSING ITEM JSON: Did you format the item parameters correctly?\n", e);
+            break;
+          }
         }
-        callback(null, postContent);
+
+        str += "</div>";
+
+        return str;
+    }
+
+    Carousel.parse = function(data, callback) {
+        if (!data || !data.postData || !data.postData.content) {
+          return callback(null, data);
+        }
+
+        if (data.postData.content.match(carRegex)) {
+          data.postData.content =
+            data.postData.content.replace(carRegex, generateCarousel(data.postData.content));
+        }
+
+        callback(null, data);
     };
 
     module.exports = Carousel;
--- static/lib.js
+++ static/lib.js
@@ -3,17 +3,21 @@
  */
 $(document).ready(function() {
     $(window).on('action:topic.loaded', function () {
-        console.log('loaded');
         var carousel = $(".carousel");
         var config = '';
+        console.log(carousel);
         try{
-            config = carousel.attr('config');
-            config = jQuery.parseJSON(config);
+          config = carousel.attr('config').replace(/\'/g, "\"");
+          console.log(config);
+          config = jQuery.parseJSON(config);
         }catch(e){
-            console.log(e);
+          console.log(e);
         }
         if(config){
             carousel.owlCarousel(config);
+        }
+        else {
+          carousel.owlCarousel();
         }
     });
 });
